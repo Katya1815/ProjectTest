2b18a582fef0bb28581f732422c12205cc5525ce
ОБПОУ КГПК <kgpk2@outlook.com>
2026-01-26 14:37:14 +0300

    1

--- Restoran/Restoran/obj/Debug/net8.0-windows/Restoran.AssemblyInfo.cs
+++ Restoran/Restoran/obj/Debug/net8.0-windows/Restoran.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restoran")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a78b3c2d0cd493406f34ed22660b0600d943e7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restoran")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restoran")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
